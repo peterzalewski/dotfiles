1910f9e9d37c3b1da46548218787cffd3f9b35c7
Peter Zalewski <pzalewski@hioscar.com>
2023-06-14 21:58:08 -0400

    Command to patch font to my liking

---
+++ patchfont.sh
@@ -0,0 +1,2 @@
+ docker run --rm -v ~/downloads/OperatorMono:/in -v ~/downloads/Patched/:/out nerdfonts/patcher --dry --careful --fontawesome --fontawesomeextension --octicons --fontlogos --fontlinux --codicons --powersymbols --pomicons --powerline --powerlineextra --mdi --weather
+
