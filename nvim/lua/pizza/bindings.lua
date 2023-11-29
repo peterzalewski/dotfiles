0cfce82900da637c7ac3678c62a7655c8e875cb7
Peter Zalewski <peter@zalewski.com>
2023-11-29 15:48:14 -0500

    Use arrows to resize splits and windows

--- nvim/lua/pizza/bindings.lua
+++ nvim/lua/pizza/bindings.lua
@@ -12,11 +12,21 @@ vim.keymap.set("n", "[z", "zk", { remap = true, desc = "Previous fold" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic item" })
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic item" })
 
--- Disable arrow key navigation to impress... who?
-vim.keymap.set({ "n", "i" }, "<up>", "<nop>")
-vim.keymap.set({ "n", "i" }, "<down>", "<nop>")
-vim.keymap.set({ "n", "i" }, "<left>", "<nop>")
-vim.keymap.set({ "n", "i" }, "<right>", "<nop>")
+-- Disable arrow key navigation in insert mode
+vim.keymap.set("i", "<up>", "<nop>")
+vim.keymap.set("i", "<down>", "<nop>")
+vim.keymap.set("i", "<left>", "<nop>")
+vim.keymap.set("i", "<right>", "<nop>")
+
+-- But use them to resize splits in normal mode
+vim.keymap.set("n", "<up>", "<Cmd>resize +10<CR>", { silent = true, desc = "Increase window height" })
+vim.keymap.set("n", "<down>", "<Cmd>resize -10<CR>", { silent = true, desc = "Decrease window height" })
+vim.keymap.set("n", "<right>", "<Cmd>vertical resize +10<CR>", { silent = true, desc = "Increase window width" })
+vim.keymap.set("n", "<left>", "<Cmd>vertical resize -10<CR>", { silent = true, desc = "Decrease window width" })
+vim.keymap.set("n", "<S-up>", "<Cmd>resize +1<CR>", { silent = true, desc = "Increase window height" })
+vim.keymap.set("n", "<S-down>", "<Cmd>resize -1<CR>", { silent = true, desc = "Decrease window height" })
+vim.keymap.set("n", "<S-right>", "<Cmd>vertical resize +1<CR>", { silent = true, desc = "Increase window width" })
+vim.keymap.set("n", "<S-left>", "<Cmd>vertical resize -1<CR>", { silent = true, desc = "Decrease window width" })
 
 vim.keymap.set("n", "[<space>", "O<esc>j", { silent = true, desc = "Insert a blank line above" })
 vim.keymap.set("n", "]<space>", "o<esc>k", { silent = true, desc = "Insert a blank line below" })
