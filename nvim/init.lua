30009172373a1288763ead2f9d33b16546327ac6
Peter Zalewski <peter@zalewski.com>
2024-04-07 02:52:34 +0000

    chore: format and remove more unneeded thingies

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,33 +1,35 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+   vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable",
+      lazypath,
+   })
 end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = ","
 
 require("lazy").setup({
-	spec = {
-		{ import = "pizza" },
-	},
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				"gzip",
-				"netrwPlugin",
-				"tarPlugin",
-				"zipPlugin",
-			},
-		},
-	},
-	ui = {
-		border = "single",
-	},
+   spec = {
+      { import = "pizza" },
+   },
+   performance = {
+      rtp = {
+         disabled_plugins = {
+            "gzip",
+            "netrwPlugin",
+            "tarPlugin",
+            "tohtml",
+            "tutor",
+            "zipPlugin",
+         },
+      },
+   },
+   ui = {
+      border = "single",
+   },
 })
