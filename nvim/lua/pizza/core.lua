9e897b75ef004170d5d11899b2db97acb671c7d0
Peter Zalewski <pzalewski@hioscar.com>
2024-09-24 13:14:14 -0400

    feat: add leap-like motion

--- nvim/lua/pizza/core.lua
+++ nvim/lua/pizza/core.lua
@@ -123,26 +123,25 @@ return {
    {
       'axkirillov/hbac.nvim',
       config = true,
-   }
-   -- {
-   --   'smoka7/hop.nvim',
-   --   version = "*",
-   --   opts = {},
-   --   init = function ()
-   --     local hop = require('hop')
-   --     local directions = require('hop.hint').HintDirection
-   --     vim.keymap.set('', 'f', function()
-   --       hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-   --     end, {remap=true})
-   --     vim.keymap.set('', 'F', function()
-   --       hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-   --     end, {remap=true})
-   --     vim.keymap.set('', 't', function()
-   --       hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-   --     end, {remap=true})
-   --     vim.keymap.set('', 'T', function()
-   --       hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-   --     end, {remap=true})
-   --   end
-   -- }
+   },
+   {
+      "folke/flash.nvim",
+      event = "VeryLazy",
+      ---@type Flash.Config
+      opts = {
+         modes = {
+            char = {
+               enabled = false,
+            },
+         },
+      },
+      -- stylua: ignore
+      keys = {
+         { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+         { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+         { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+         { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+         { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      },
+   },
 }
